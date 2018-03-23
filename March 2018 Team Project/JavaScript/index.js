e77c786043c551e2e18c47c442e6b4f5af3810e5
NoelMelia <G00237491@gmit.ie>
2018-03-23 15:08:41 +0000

    pull new master

--- March 2018 Team Project/JavaScript/index.js
+++ March 2018 Team Project/JavaScript/index.js
@@ -1,20 +1,8 @@
-//A. Moore, D. Lally, N. Melia 
+//A. Moore. D, Lally, N. Melia 
 //index.js - 1st yr final project
 
-var slideIndex = 0;
-showSlides();
+alert("Test");
 
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; 
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1} 
-    slides[slideIndex-1].style.display = "block"; 
-    setTimeout(showSlides, 4600); // Change image every 4.6 seconds
-}
 	
 
 	
