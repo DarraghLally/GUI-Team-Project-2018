bc4b33dc38966b1df8d84e8b8b64216dbe8664a9
NoelMelia <G00237491@gmit.ie>
2018-03-23 13:43:11 +0000

    Updated again

---
+++ March 2018 Team Project/JavaScript/login.js
@@ -0,0 +1,32 @@
+//A. Moore. D, Lally, N. Melia 
+//login.js - 1st yr final project
+
+
+	//make an array of objects with JSON
+	var accounts = 
+	[
+		{
+			"username" : "John Doe", 
+			"pssword" : "maryhadalittlelamb", 
+		},
+		{
+			"username" : "Mary Doe", 
+			"pssword" : "timmy08" 
+		}
+	];
+		alert("Test");
+	
+	// Then operate on the array with a for loop 
+	for (var i = 0; i< accounts.length; i++) 
+	{
+		var name = accounts[i].username; // Or some other logic.
+		var pass = accounts[i].pssword;
+		//output city and street
+		alert(pass);
+	}
+	
+
+
+	
+
+	
