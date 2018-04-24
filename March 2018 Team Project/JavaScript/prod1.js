364354cd4e5e1ea91f88aeb1b9b444b2850bab70
Darragh Lally <37068479+DarraghLallyGMIT@users.noreply.github.com>
2018-04-24 18:57:15 +0100

    24.04.18 - NavBar

--- March 2018 Team Project/JavaScript/prod1.js
+++ March 2018 Team Project/JavaScript/prod1.js
@@ -1,6 +1,6 @@
 
 
-	/On click 'add to basket' - Getting quantity amount - Adding product ID and quantity to local storage to be pulled into Basket.html for processing
+	//On click 'add to basket' - Getting quantity amount - Adding product ID and quantity to local storage to be pulled into Basket.html for processing
 document.getElementById("addToBasket").onclick = function()
 {
 	// Retrieve
@@ -50,5 +50,15 @@ document.getElementById("addToBasket").onclick = function()
 		localStorage.setItem("basketActive", 1);
 	}
 
+	
 } 
 
+//On click 'search' - send input text to local
+	document.getElementById("searchBtn").onclick = function()
+	{	
+		//get the user name from the text box
+		var searchValue = document.getElementById("searchBarText").value;
+		
+		//send search
+		localStorage.setItem("searchBar", searchValue);
+	}
