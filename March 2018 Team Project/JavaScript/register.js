22537d6d98a187a551fa0304dcd9bdcb31570132
NoelMelia <37183863+NoelMelia@users.noreply.github.com>
2018-04-24 18:02:04 +0100

    24th April

--- March 2018 Team Project/JavaScript/register.js
+++ March 2018 Team Project/JavaScript/register.js
@@ -4,6 +4,7 @@
 //run script when submit button is pressed
 document.getElementById("regBtn").onclick = function()
 {
+	
 	//get the user details from the signup form
 	var name = document.getElementById("userName").value;
 	var email = document.getElementById("userEmail").value;
@@ -14,26 +15,31 @@ document.getElementById("regBtn").onclick = function()
 	var address3 = document.getElementById("userAddress3").value;
 	var password = document.getElementById("userPassword").value;
 	
-	//generate random client number between 70k and 10k
-	var clientNumber = Math.floor((Math.random() * 70000) + 10000);
-	
-	//build object and with the user details
-	var newAccount = 
+	if(name.length > 6  && password.length >= 8)
 	{
-		"clientId" : clientNumber,
-		"username" : name, 
-		"pssword" : password,
-		"addressLine1" : address1,
-		"addressLine2" : address2,
-		"addressLine3" : address3,
-		"email" : email,
-		"phone" : phone
-	};
-	
-	//Turn object into JSON format
-	var accountString = JSON.stringify(newAccount);
-	
-	//send to local storage
-	localStorage.setItem("accountReg", 1);
-	localStorage.setItem("newAccount", accountString);
+		//generate random client number between 70k and 10k
+		var clientNumber = Math.floor((Math.random() * 70000) + 10000);
+		
+		//build object and with the user details
+		var newAccount = 
+		{
+			"clientId" : clientNumber,
+			"username" : name, 
+			"pssword" : password,
+			"addressLine1" : address1,
+			"addressLine2" : address2,
+			"addressLine3" : address3,
+			"email" : email,
+			"phone" : phone
+		};
+		
+		//Turn object into JSON format
+		var accountString = JSON.stringify(newAccount);
+		
+		//send to local storage
+		localStorage.setItem("accountReg", 1);
+		localStorage.setItem("newAccount", accountString);
+		window.open("../index.html");
+		//test if name found
+	}
 }
