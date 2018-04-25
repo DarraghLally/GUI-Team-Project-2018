252c32ae082e15278f914be6cab02f6a7a1a93ed
NoelMelia <37183863+NoelMelia@users.noreply.github.com>
2018-04-25 21:32:07 +0100

    noelmeliatime5past

--- March 2018 Team Project/JavaScript/contact.js
+++
@@ -1,8 +0,0 @@
-function myMap() {
-  var myCenter = new google.maps.LatLng(-34.397,150.644);
-  var mapCanvas = document.getElementById("map");
-  var mapOptions = {center: myCenter, zoom: 12};
-  var map = new google.maps.Map(mapCanvas, mapOptions);
-  var marker = new google.maps.Marker({position:myCenter});
-  marker.setMap(map);
-}
